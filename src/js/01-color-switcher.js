f22f63872128dec698f47db2bd6b58ebcb450f3f
bolt2000 <bolt2000@ukr.net>
2023-03-10 16:07:52 +0200

    +files +1 dz

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+
+const body = document.querySelector("body");
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+let timerId = null;
+
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+      console.log('color',body.style.backgroundColor);
+      startBtn.disabled = true;
+  }, 1000);
+
+});
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+  console.log('Stop', body.style.backgroundColor);
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
