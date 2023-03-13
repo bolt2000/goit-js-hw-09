d685931b1d31d26ccf0e8b36381f3f4bf5971af8
bolt2000 <bolt2000@ukr.net>
2023-03-13 20:17:19 +0200

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,55 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+
+const position = document.querySelector('input[name="step"]');
+const delay = document.querySelector('input[name="delay"]');
+const amount = document.querySelector('input[name="amount"]');
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  // const shouldResolve = Math.random() > 0.3;
+  
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+  
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+        Notify.warning(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject({ position, delay });
+         Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+
+      }, delay);
+  });
+};
+
+
+
+
+
+createPromise(3, 1500, 5)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    // Notify.warning(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    // Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
